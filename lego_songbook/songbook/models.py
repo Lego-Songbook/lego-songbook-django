c21a2e3a246258752344a1649e0a523ab56325ee
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-12-30 23:56:02 +0800

    Add basic models

--- lego_songbook/songbook/models.py
+++ lego_songbook/songbook/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Song(models.Model):
+    """"""
+
+    name = models.CharField(max_length=30)
+    key = models.CharField(max_length=3)
+    sheet_type = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.name
+
+
+class Setlist(models.Model):
+    """"""
+
+    date = models.DateField()
+    worship_leader = models.CharField(max_length=30)
+    songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return str(self.date)
