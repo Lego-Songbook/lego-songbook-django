0e9f05fc73079205eab6df0019939596365bf179
Kip Yin <28321392+kipyin@users.noreply.github.com>
2020-02-02 12:18:56 +0800

    Add forget password actions

--- apps/legoworship/urls.py
+++ apps/legoworship/urls.py
@@ -15,8 +15,33 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("songbook/", include("songbook.urls")),
     path("admin/", admin.site.urls),
+
+    # password reset feature:
+    # https://docs.djangoproject.com/en/3.0/ref/contrib/admin/#adding-a-password
+    # -reset-feature
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
 ]
