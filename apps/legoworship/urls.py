fcb963f7ef8214693be407ee94bcf134f8eb289b
Kip Yin <28321392+kipyin@users.noreply.github.com>
2020-02-03 16:39:18 +0800

    Lint

--- apps/legoworship/urls.py
+++ apps/legoworship/urls.py
@@ -14,34 +14,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
 urlpatterns = [
     path("songbook/", include("songbook.urls")),
     path("admin/", admin.site.urls),
-
     # password reset feature:
     # https://docs.djangoproject.com/en/3.0/ref/contrib/admin/#adding-a-password
     # -reset-feature
     path(
-        'admin/password_reset/',
+        "admin/password_reset/",
         auth_views.PasswordResetView.as_view(),
-        name='admin_password_reset',
+        name="admin_password_reset",
     ),
     path(
-        'admin/password_reset/done/',
+        "admin/password_reset/done/",
         auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done',
+        name="password_reset_done",
     ),
     path(
-        'reset/<uidb64>/<token>/',
+        "reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm',
+        name="password_reset_confirm",
     ),
     path(
-        'reset/done/',
+        "reset/done/",
         auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete',
+        name="password_reset_complete",
     ),
 ]
