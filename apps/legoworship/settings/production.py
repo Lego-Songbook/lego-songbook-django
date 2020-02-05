11b7c4b1299de35e00bc63392a4c31ed8ab28d83
Kip Yin <28321392+kipyin@users.noreply.github.com>
2020-02-05 17:17:39 +0800

    Lint

--- apps/legoworship/settings/production.py
+++ apps/legoworship/settings/production.py
@@ -1,8 +1,7 @@
-
 from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
